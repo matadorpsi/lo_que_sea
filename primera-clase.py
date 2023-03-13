f309c57e19c7955f7cb78cc20c986b340ea00ec3
matadorpsi <124622675+matadorpsi@users.noreply.github.com>
2023-03-13 12:48:29 -0500

    Add files via upload

---
+++ primera-clase.py
@@ -0,0 +1,12 @@
+def suma(a, b):
+    return a + b
+
+def resta(a, b):
+    return a - b
+
+def multiplicacion(a, b):
+    return a * b
+
+# Ejemplo de uso
+resultado = suma(2, 3)
+print(resultado)  # Output: 5
